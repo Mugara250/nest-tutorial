68057e072f84481f78f0b725ba3c9ff3fd943d3c
Mugara250 <mbonyumugara52@gmail.com>
2026-01-24 01:08:44 +0200

    authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1m' },
+      }),
+    }),
+    UsersModule,
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,8 @@
-import {
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { users } from 'src/data/data';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 type AuthInput = { username: string; password: string };
 type SignInData = { userID: number; username: string };
@@ -11,18 +10,19 @@ type AuthResult = { accessToken: string; userID: number; username: string };
 
 @Injectable()
 export class AuthService {
-  public constructor(private readonly usersService: UsersService) {}
+  public constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService
+  ) {}
   async authenticate(input: AuthInput): Promise<AuthResult> {
     const user = await this.validate(input);
 
     if (!user) {
       throw new UnauthorizedException('Not authenticated');
     }
-    return {
-      accessToken: 'afkjadlfjlafjldaksjfdakjfadjfkda',
-      userID: user.userID,
-      username: user.username,
-    };
+
+    return await this.signIn(user);
   }
   async validate(input: AuthInput): Promise<SignInData | undefined> {
     const user = await this.usersService.findUserByName(input.username);
@@ -33,5 +33,16 @@ export class AuthService {
     if (user && password === input.password) {
       return user;
     }
+  }
+
+  async signIn(user: SignInData): Promise<AuthResult> {
+    const tokenPayload = {
+      sub: user.userID,
+      username: user.username,
+    };
+
+    const accessToken = await this.jwtService.signAsync(tokenPayload);
+
+    return { accessToken, userID: user.userID, username: user.username };
   }
 }
